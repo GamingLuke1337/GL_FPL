0a99c6d6d6d07e198ad04b31033aff2e980f4c53
GamingLuke1337 <64887613+GamingLuke1337@users.noreply.github.com>
2024-10-27 18:35:41 +0100

    Add files via upload

---
+++ FPL/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Gamingluke1337'
+description 'First Person Camera Lock on Shooting and Aiming'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
